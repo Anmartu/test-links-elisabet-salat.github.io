805384a2146ad6b3b45ad64a492c896cc382ada8
Andreu Martorell <anmartu@gmail.com>
2023-04-12 13:40:12 +0200

    12/4--4

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ export const [Contact, Contact_es, Contact_en] = arr.map(() => {
     <div className='contact-container'>
       <h1 className='contact-title'>{ind[lng].contact}</h1>
       <a className='contact-mail' href="mailto:elisabetsalat@yahoo.es">elisabetsalat@yahoo.es</a>
-      <div className='xarxes contact'>
+      {/* <div className='xarxes contact'>
         <ul>
             <li>
               <a className='social' href="https://www.instagram.com/andreu.martorell/"><Facebook/> </a>
@@ -18,7 +18,7 @@ export const [Contact, Contact_es, Contact_en] = arr.map(() => {
               <a className='social' href="https://www.facebook.com/Andreu-Martorell-101890385143452"><Insta/>  </a>
             </li>
           </ul>
-      </div>
+      </div> */}
     </div>
     </>
   )}
